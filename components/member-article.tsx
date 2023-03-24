fecc49f62d490a7f49dc0a54207effd915fdca29
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-03-25 00:36:39 +0800

    Change back chat gpt suggestions. It was wrong.

--- components/member-article.tsx
+++ components/member-article.tsx
@@ -13,33 +13,28 @@ export default function MemberArticle({ member, align }: Props) {
   const { width } = useWindowSize();
   const isMobile = width < 1280;
   align = isMobile ? 'left' : align;
-
-  const renderMemberImg = () => (
-    <MemberImg src={member.photo} alt={member.alt} />
-  );
-
-  const renderAuthorTag = () => (
-    <AuthorTag name={member.name} title={member.position} position={align} />
-  );
-
-  const renderStatement = () => (
-    <p
-      className={`font-body lg:text-xl text-lg leading-[1.75rem] px-3 text-center ${
-        align === 'left' ? 'lg:text-left' : 'lg:text-right'
-      }`}
-    >
-      {member.statement}
-    </p>
-  );
-
   return (
     <article className='lg:py-16 py-12 lg:px-[24%] md:px-[18%] px-[10%] flex flex-col xl:flex-row justify-between gap-6 lg:gap-8 align-middle'>
-      {align === 'left' && !isMobile && renderMemberImg()}
+      {(align === 'left' && !isMobile) || (
+        <MemberImg src={member.photo} alt={member.alt} />
+      )}
       <div className='flex flex-col justify-center gap-8'>
-        {renderStatement()}
-        {renderAuthorTag()}
+        <p
+          className={`font-body lg:text-xl text-lg leading-[1.75rem] px-3 text-center ${
+            align === 'left' ? 'lg:text-left' : 'lg:text-right'
+          }`}
+        >
+          {member.statement}
+        </p>
+        <AuthorTag
+          name={member.name}
+          title={member.position}
+          position={align}
+        />
       </div>
-      {!(align === 'left') && !isMobile && renderMemberImg()}
+      {!(align === 'left') || isMobile || (
+        <MemberImg src={member.photo} alt={member.alt} />
+      )}
     </article>
   );
 }
