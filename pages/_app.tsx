556fc55493d51ade52ca6e9b8aed53c5c18a70db
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-03-27 23:41:51 +0800

    Add link tag to add favicon to tab.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import '../styles/index.scss';
 import Footer from '../components/footer';
 import { usePreserveScroll } from '../hooks/usePreserveScroll';
 import { Analytics } from '@vercel/analytics/react';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   usePreserveScroll();
   return (
     <div className='bg-bg_paper'>
+      <Head>
+        <link rel='shortcut icon' href='/assets/Images/favicon.png' />
+      </Head>
       <Component {...pageProps} />
       <Footer />
       <Analytics />
