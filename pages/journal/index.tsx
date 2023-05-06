4ab210ca14e362c0723f381c810dd5a78393c133
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-05-06 13:05:56 +0800

    Query strapi for journal cover image src.

--- pages/journal/index.tsx
+++ pages/journal/index.tsx
@@ -4,14 +4,15 @@ import Navbar from '../../components/navbar';
 import PostType from '../../interfaces/PostType';
 import PostPreview from '../../components/post-preview';
 import LandingTitle from '../../components/landing-title';
-import { GET_ALL_POSTS } from '../../graphql/queries';
+import { GET_ALL_POSTS, GET_JOURNAL_COVER_IMG } from '../../graphql/queries';
 import client from '../../graphql/apollo-client';
 
 type Props = {
   posts: PostType[];
+  coverImgSrc: string;
 };
 
-export default function JournalPage({ posts }: Props) {
+export default function JournalPage({ posts, coverImgSrc }: Props) {
   return (
     <>
       <Navbar />
@@ -21,7 +22,7 @@ export default function JournalPage({ posts }: Props) {
       <main className='min-h-screen'>
         <section id='journal-landing-img' className={`h-[100vh] mb-16`}>
           <CoverImage
-            src='BoatAtJetty.png'
+            src={coverImgSrc}
             height='100vh'
             alt='Boat parked at Pulau Ubin jetty with background of the shore'
           />
@@ -39,6 +40,25 @@ export default function JournalPage({ posts }: Props) {
 
 // todo: Pagination for post page. Current just show all posts
 export async function getStaticProps() {
+  const {
+    data: {
+      journalMedia: {
+        data: {
+          attributes: {
+            Cover_img: {
+              data: {
+                attributes: { url },
+              },
+            },
+          },
+        },
+      },
+    },
+  } = await client.query({
+    query: GET_JOURNAL_COVER_IMG,
+  });
+  console.log(url);
+
   const {
     data: {
       posts: { data: PostDataArr },
@@ -67,6 +87,6 @@ export async function getStaticProps() {
     };
   });
   return {
-    props: { posts },
+    props: { posts, coverImgSrc: url },
   };
 }
