2d4f6dd5f011e2e2894e45513e1cad9a6196e53a
Jin_Han <jinhan.loh.2021@scis.smu.edu.sg>
2023-06-10 15:56:43 +0800

    Comment. Create branch for content preview.

--- graphql/apollo-client.ts
+++ graphql/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: process.env.STRAPI_PULIC_API_URL || 'http://localhost:1337/graphql',
+  uri: process.env.STRAPI_PULIC_API_URL || 'http://localhost:1337/graphql', // set to local host for development
   cache: new InMemoryCache(),
 });
 
