fe9870908c96083e21173d34edb22b0e734abe63
unknown <jinhan.loh.2021@scis.smu.edu.sg>
2023-10-16 13:38:28 +0800

    Mostly done with the frontend design, but issues with CMS writing data to postgres.

--- pages/instagram/index.tsx
+++ pages/instagram/index.tsx
@@ -3,17 +3,25 @@ import client from '../../graphql/apollo-client';
 import { GET_ALL_INSTA } from '../../graphql/queries';
 import { InstagramPost } from '../../interfaces/InstagramPost';
 import InstaCard from '../../components/insta-card';
+import Navbar from '../../components/navbar';
+import Head from 'next/head';
 type Props = {
   instaPosts: InstagramPost[];
 };
 
 export default function InstagramPage({ instaPosts }: Props) {
   return (
-    <main className='grid grid-cols-3 place-items-center w-[90vw] sm:w-[72vw] md:w-[54vw] lg:w-[36vw] mx-auto gap-y-1 my-8'>
-      {instaPosts.map((e, i) => (
-        <InstaCard url={e.postUrl} src={e.imgUrl} key={i} />
-      ))}
-    </main>
+    <>
+      <Navbar />
+      <Head>
+        <title>Ubin Kakis - Instagram</title>
+      </Head>
+      <main className='grid grid-cols-3 place-items-center w-[90vw] sm:w-[72vw] md:w-[54vw] lg:w-[36vw] mx-auto gap-x-3 gap-y-1 mt-24 mb-12'>
+        {instaPosts.map((e, i) => (
+          <InstaCard url={e.postUrl} src={e.imgUrl} key={i} />
+        ))}
+      </main>
+    </>
   );
 }
 
@@ -31,6 +39,11 @@ export async function getStaticProps() {
       imgUrl: e.attributes.CoverImage.data.attributes.url,
       date: e.attributes.Date,
     };
+  });
+  instaPosts.sort((a, b) => {
+    const timeA = new Date(a.date).getTime();
+    const timeB = new Date(b.date).getTime();
+    return timeB - timeA;
   });
 
   return { props: { instaPosts } };
